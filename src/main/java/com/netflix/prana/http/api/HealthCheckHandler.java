a34b4ca9bde12dae9c3e6bc24bb095f44914bca0
Diptanu Choudhury <diptanuc@gmail.com>
2014-10-28 16:08:29 -0700

    Added a Test for HealthCheckHandler

--- src/main/java/com/netflix/prana/http/api/HealthCheckHandler.java
+++ src/main/java/com/netflix/prana/http/api/HealthCheckHandler.java
@@ -23,40 +23,15 @@ import java.net.URL;
 
 public class HealthCheckHandler implements RequestHandler<ByteBuf, ByteBuf> {
 
-    private final String externalHealthCheckURL = DynamicProperty.getInstance("prana.host.healthcheck.url").getString("http://localhost:7001/healthcheck");
-
-    private final HttpClient<ByteBuf, ByteBuf> httpClient;
-
-    private String path = "";
-
-    public HealthCheckHandler() {
-        PipelineConfigurator<HttpClientResponse<ByteBuf>, HttpClientRequest<ByteBuf>> pipelineConfig
-                = PipelineConfigurators.httpClientConfigurator();
-
-        String host = "localhost";
-        int port = 7101;
-        try {
-            URL url = new URL(externalHealthCheckURL);
-            host = url.getHost();
-            port = url.getPort();
-            path = url.getPath();
-        } catch (MalformedURLException e) {
-            //continue
-        }
-        httpClient = RxNetty.<ByteBuf, ByteBuf>newHttpClientBuilder(host, port)
-                .pipelineConfigurator(pipelineConfig)
-                .channelOption(ChannelOption.CONNECT_TIMEOUT_MILLIS, 2000)
-                .build();
-    }
-
-    @Override
     public Observable<Void> handle(HttpServerRequest<ByteBuf> serverRequest, final HttpServerResponse<ByteBuf> serverResponse) {
+        String externalHealthCheckURL = DynamicProperty.getInstance("prana.host.healthcheck.url").getString("http://localhost:7001/healthcheck");
         serverResponse.getHeaders().add("Content-Type", "application/xml");
         if (Strings.isNullOrEmpty(externalHealthCheckURL)) {
             serverResponse.writeBytes("<health>ok</health>".getBytes());
+            return serverResponse.close();
         }
 
-        return httpClient.submit(HttpClientRequest.createGet(path)).flatMap(new Func1<HttpClientResponse<ByteBuf>, Observable<Void>>() {
+        return getResponse(externalHealthCheckURL).flatMap(new Func1<HttpClientResponse<ByteBuf>, Observable<Void>>() {
             @Override
             public Observable<Void> call(HttpClientResponse<ByteBuf> response) {
                 if (response.getStatus().code() == 200) {
@@ -74,4 +49,25 @@ public class HealthCheckHandler implements RequestHandler<ByteBuf, ByteBuf> {
             }
         });
     }
+
+    private Observable<HttpClientResponse<ByteBuf>> getResponse(String externalHealthCheckURL) {
+        String host = "localhost";
+        int port = 7101;
+        String path = "/healthcheck";
+        try {
+            URL url = new URL(externalHealthCheckURL);
+            host = url.getHost();
+            port = url.getPort();
+            path = url.getPath();
+        } catch (MalformedURLException e) {
+            //continue
+        }
+        HttpClient<ByteBuf, ByteBuf> httpClient = RxNetty.<ByteBuf, ByteBuf>newHttpClientBuilder(host, port)
+                .pipelineConfigurator(PipelineConfigurators.httpClientConfigurator())
+                .channelOption(ChannelOption.CONNECT_TIMEOUT_MILLIS, 2000)
+                .build();
+        return httpClient.submit(HttpClientRequest.createGet(path));
+
+    }
+
 }
---
+++ src/test/java/HealthCheckHandlerTest.java
@@ -0,0 +1,79 @@
+import com.netflix.prana.http.api.HealthCheckHandler;
+import com.netflix.prana.http.api.PingHandler;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelOption;
+import io.netty.handler.codec.http.HttpResponseStatus;
+import io.reactivex.netty.RxNetty;
+import io.reactivex.netty.pipeline.PipelineConfigurators;
+import io.reactivex.netty.protocol.http.client.HttpClient;
+import io.reactivex.netty.protocol.http.client.HttpClientRequest;
+import io.reactivex.netty.protocol.http.server.HttpServer;
+import io.reactivex.netty.protocol.http.server.HttpServerRequest;
+import io.reactivex.netty.protocol.http.server.HttpServerResponse;
+import io.reactivex.netty.protocol.http.server.RequestHandler;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import rx.Observable;
+
+public class HealthCheckHandlerTest {
+
+    private HttpServer<ByteBuf, ByteBuf> server;
+    private HttpServer<ByteBuf, ByteBuf> externalServer;
+
+    private HttpClient<ByteBuf, ByteBuf> client;
+
+    private final int port = 23455;
+
+    private final int externalServerPort = 23457;
+
+    @Before
+    public void setUp() {
+        server = RxNetty.newHttpServerBuilder(port, new HealthCheckHandler())
+                .pipelineConfigurator(PipelineConfigurators.<ByteBuf, ByteBuf>httpServerConfigurator()).build();
+        server.start();
+
+        externalServer = RxNetty.newHttpServerBuilder(externalServerPort, new ExternalServerHandler())
+                .pipelineConfigurator(PipelineConfigurators.<ByteBuf, ByteBuf>httpServerConfigurator()).build();
+        externalServer.start();
+
+        client = RxNetty.<ByteBuf, ByteBuf>newHttpClientBuilder("localhost", port)
+                .pipelineConfigurator(PipelineConfigurators.httpClientConfigurator())
+                .channelOption(ChannelOption.CONNECT_TIMEOUT_MILLIS, 2000)
+                .build();
+
+    }
+
+
+    @After
+    public void tearDown() throws InterruptedException {
+        server.shutdown();
+        externalServer.shutdown();
+    }
+
+
+    @Test
+    public void shouldPingExternalHostsForHealthCheck() {
+        System.setProperty("prana.host.healthcheck.url", "http://localhost:" + externalServerPort);
+        HttpClientRequest<ByteBuf> request = HttpClientRequest.<ByteBuf>createGet("/healthcheck");
+        Assert.assertEquals("<health>ok</health>", Utils.getResponse(request, client));
+    }
+
+    @Test
+    public void shouldReturnOkIfHealthCheckURLIsSetToNull() {
+        System.setProperty("prana.host.healthcheck.url", "");
+        HttpClientRequest<ByteBuf> request = HttpClientRequest.<ByteBuf>createGet("/healthcheck");
+        Assert.assertEquals("<health>ok</health>", Utils.getResponse(request, client));
+    }
+
+
+    private class ExternalServerHandler implements RequestHandler<ByteBuf, ByteBuf> {
+
+        @Override
+        public Observable<Void> handle(HttpServerRequest<ByteBuf> request, HttpServerResponse<ByteBuf> response) {
+            response.setStatus(HttpResponseStatus.OK);
+            return response.close();
+        }
+    }
+}
