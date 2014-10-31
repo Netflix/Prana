3912f923aabe0085868d6ebe0095de01ee2a6502
Diptanu Choudhury <diptanuc@gmail.com>
2014-10-30 20:48:51 -0700

    Removed un-used config parameters

--- src/main/java/com/netflix/prana/config/PranaConfig.java
+++ src/main/java/com/netflix/prana/config/PranaConfig.java
@@ -7,32 +7,11 @@ public class PranaConfig {
     @Option(name = "-p", aliases = "--http-port-api", usage = "Http Port of Prana Server")
     private int httpPort = 8078;
     
-    @Option(name = "-b", aliases = "--http-bind-ip", usage = "Hostname or IP address to bind")
-    private String bindHost = "0.0.0.0";
-    
-    @Option(name = "-w", aliases = "--http-worker-max", usage = "Max http worker count")
-    private int workerMax = 10;
-    
     @Option(name = "-a", aliases = "--app-name", usage = "Parent host application name")
     private String appName = "prana";
     
-    @Option(name = "-h", aliases = "--base-dir", usage = "Prana install dir")
-    private String pranaHome = System.getProperty("user.home");
-    
-    public int getWorkerMax() {
-        return workerMax;
-    }
-
     public int getHttpPort() {
         return httpPort;
-    }
-
-    public String getBindHost() {
-        return bindHost;
-    }
-    
-    public String getPranaHome() {
-        return pranaHome;
     }
 
     public String getAppName() {
